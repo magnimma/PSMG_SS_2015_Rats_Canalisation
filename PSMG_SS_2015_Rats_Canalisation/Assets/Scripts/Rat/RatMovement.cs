c1ddaee8d813676d5ec70afba2c482549af094a1
krisk1 <christian.kugler@stud.uni-regensburg.de>
2015-04-20 20:54:20 +0200

    RatMovement und CameraFollow
    RatMovement mit Pfeiltasten oder WASD
    Springen Space
    Kamera followed der Ratte Smooth und reagiert auf Collision mit anderen
    Meshes (Wänden)

---
+++ PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/Rat/RatMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RatMovement : MonoBehaviour {
+
+    public float rotationSpeed = 1f;
+    public float movementSpeed = 10f;
+    public float jumpSpeed = 20f;
+    public float RayCastLength = .1f;
+    public bool isGrounded;
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded){
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed);
+        }
+
+
+        checkIfGrounded();
+        Move(verticalInput);
+        if (isGrounded)
+        {
+            Turn(horizontalInput);
+        }
+        
+        
+    }
+
+
+    private void checkIfGrounded()
+    {
+        if (Physics.Raycast(transform.position, new Vector3(0, -.1f, 0), RayCastLength))
+        {
+            isGrounded = true;
+        }
+        else isGrounded = false;
+    }
+
+    private void Move(float inputSignal)
+    {
+        Vector3 newPosition = transform.forward.normalized * inputSignal * movementSpeed * Time.deltaTime;
+        
+        GetComponent<Rigidbody>().MovePosition(transform.position + newPosition);
+    }
+
+    private void Turn(float inputSignal)
+    {
+        float angle = inputSignal * rotationSpeed;
+        transform.Rotate(transform.up * angle);
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ PSMG_SS_2015_Rats_Canalisation/Assets/Scripts/Rat/SmoothFollowCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollowCamera : MonoBehaviour
+{
+
+    // The target we are following
+    public Transform target;
+    // The distance in the x-z plane to the target
+    public float distance = 10.0f;
+    // the height we want the camera to be above the target
+    public float height = 5.0f;
+    // How much we
+    public float Damping = 4.0f;
+
+    void LateUpdate()
+    {
+        // Early out if we don't have a target
+        if (!target)
+            return;
+
+        Vector3 CameraFinalPosition = new Vector3();
+        Vector3 DirPlayerToCamera = new Vector3();
+
+        CameraFinalPosition = target.position - (target.forward * distance);
+        CameraFinalPosition += Vector3.up * height;
+
+        DirPlayerToCamera = CameraFinalPosition - target.position;
+        DirPlayerToCamera.Normalize();
+
+        Ray MyRay = new Ray(target.position, DirPlayerToCamera);
+        RaycastHit HitInfo = new RaycastHit();
+        Physics.Raycast(MyRay, out HitInfo, distance);
+        if (HitInfo.collider != null)
+        {
+            if (HitInfo.distance < distance)
+            {
+                CameraFinalPosition += -DirPlayerToCamera * (distance - HitInfo.distance);
+            }
+        }
+
+        transform.position = Vector3.Lerp(transform.position, CameraFinalPosition, Time.deltaTime * Damping);
+        transform.LookAt(target);
+    }
+}
